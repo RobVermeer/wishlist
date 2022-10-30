0239f8c85a31624329b2629ea7ff120a986f56dc
RobVermeer <rob@vermeertech.nl>
2022-10-30 19:36:47 +0100

    Order users in admin

--- lib/users/getUsers.ts
+++ lib/users/getUsers.ts
@@ -1,7 +1,9 @@
 import prisma from "~/lib/prisma"
 
 export const getUsers = async () => {
-  const data = await prisma.user.findMany()
+  const data = await prisma.user.findMany({
+    orderBy: { name: "asc" },
+  })
 
   return data
 }
