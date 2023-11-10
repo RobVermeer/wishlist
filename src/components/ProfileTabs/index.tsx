22cd0179f9173a97497ac324d5921ef17ed87b7b
RobVermeer <rob@vermeertech.nl>
2023-11-10 23:16:36 +0100

    Change scroll to ScrollArea

--- src/components/ProfileTabs/index.tsx
+++ src/components/ProfileTabs/index.tsx
@@ -4,35 +4,45 @@ import { usePathname } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { BookUser, Group, List } from "lucide-react"
+import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 
 export const ProfileTabs = () => {
   const pathname = usePathname()
 
   return (
-    <div className="flex gap-2 overflow-x-scroll">
-      <Button variant={pathname === "/profile" ? "default" : "outline"} asChild>
-        <Link href="/profile">
-          <List size="16" className="mr-2" /> Verlanglijstjes
-        </Link>
-      </Button>
-      <Button
-        variant={pathname === "/profile/groups" ? "default" : "outline"}
-        asChild
-      >
-        <Link href="/profile/groups">
-          <Group size="16" className="mr-2" />
-          Groepen
-        </Link>
-      </Button>
-      <Button
-        variant={pathname === "/profile/bought" ? "default" : "outline"}
-        asChild
-      >
-        <Link href="/profile/bought">
-          <BookUser size="16" className="mr-2" />
-          Gekochte cadeaus
-        </Link>
-      </Button>
-    </div>
+    <ScrollArea className="whitespace-nowrap">
+      <div className="flex gap-2">
+        <Button
+          className="shrink-0"
+          variant={pathname === "/profile" ? "default" : "outline"}
+          asChild
+        >
+          <Link href="/profile">
+            <List size="16" className="mr-2" /> Verlanglijstjes
+          </Link>
+        </Button>
+        <Button
+          className="shrink-0"
+          variant={pathname === "/profile/groups" ? "default" : "outline"}
+          asChild
+        >
+          <Link href="/profile/groups">
+            <Group size="16" className="mr-2" />
+            Groepen
+          </Link>
+        </Button>
+        <Button
+          className="shrink-0"
+          variant={pathname === "/profile/bought" ? "default" : "outline"}
+          asChild
+        >
+          <Link href="/profile/bought">
+            <BookUser size="16" className="mr-2" />
+            Gekochte cadeaus
+          </Link>
+        </Button>
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   )
 }
