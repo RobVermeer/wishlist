2db02c98f8ba9ef83765818a88264379cf5d5214
RobVermeer <rob@vermeertech.nl>
2023-11-29 22:25:31 +0100

    Fix server translations function

--- src/lib/users/getUsers.ts
+++ src/lib/users/getUsers.ts
@@ -3,12 +3,12 @@
 import { authOptions } from "@/app/[locale]/api/auth/[...nextauth]/route"
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth"
-import { useTranslations } from "next-intl"
+import { getTranslations } from "next-intl/server"
 import { cache } from "react"
 
 export const getUsers = cache(async () => {
   const session = await getServerSession(authOptions)
-  const t = useTranslations("Errors")
+  const t = await getTranslations("Errors")
 
   if (!session) {
     throw new Error(t("notLoggedIn"))
