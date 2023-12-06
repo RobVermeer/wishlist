4fe1105b0cb7fa0b3d0ea08d7ec4b86bed1f4ebf
RobVermeer <rob@vermeertech.nl>
2023-12-06 17:27:34 +0100

    Try our suspense without metadata

--- src/app/[locale]/wishlist/[id]/page.tsx
+++ src/app/[locale]/wishlist/[id]/page.tsx
@@ -1,24 +1,9 @@
-import { getWishlistById } from "@/lib/wishlists/getWishlistById"
-import { Metadata } from "next"
-import { getTranslations } from "next-intl/server"
 import { Wishlist } from "@/components/Wishlist"
 import { Suspense } from "react"
 import { WishlistSkeleton } from "@/components/WishlistSkeleton/index."
 
 interface Props {
   params: { id: string; locale: string }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { id, locale } = params
-  const t = await getTranslations({ locale, namespace: "Common" })
-  const wishlist = await getWishlistById(id, true)
-
-  if (!wishlist) return {}
-
-  return {
-    title: `${wishlist.title || t("myList")} - Wishlist`,
-  }
 }
 
 export default async function WishlistPage({ params }: Props) {
