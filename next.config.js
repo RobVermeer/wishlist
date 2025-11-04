a5582d4131f7c574c7202c47de338b3c12b4f98c
Rob Vermeer <rob@vermeertech.nl>
2025-11-04 21:54:57 +0100

    Fix generate prisma client

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withNextIntl = require("next-intl/plugin")()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    outputFileTracingIncludes: {
+      "/*": [
+        "./node_modules/@prisma/client/**/*",
+        "./node_modules/.prisma/client/**/*",
+      ],
+    },
+  },
 }
 
 module.exports = withNextIntl(nextConfig)
