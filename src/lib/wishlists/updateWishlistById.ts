0d528cb7f22cce278859d352746c52d332f61a2f
Ulla <ullavansprang@gmail.com>
2025-11-04 21:33:13 +0100

    feat: add ability to edit wishlist description

--- src/lib/wishlists/updateWishlistById.ts
+++ src/lib/wishlists/updateWishlistById.ts
@@ -1,26 +1,26 @@
-"use server"
+"use server";
 
-import { prisma } from "@/lib/prisma"
-import { wishlistProperties } from "./publicProperties"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/nextAuth"
-import { revalidatePath } from "next/cache"
-import { wishlistSchema } from "@/lib/schema"
-import { z } from "zod"
-import { getErrorMessage } from "@/lib/utils"
-import { getTranslations } from "next-intl/server"
-import { trackIssue } from "@/lib/trackIssue"
+import { prisma } from "@/lib/prisma";
+import { wishlistProperties } from "./publicProperties";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/nextAuth";
+import { revalidatePath } from "next/cache";
+import { wishlistSchema } from "@/lib/schema";
+import { z } from "zod";
+import { getErrorMessage } from "@/lib/utils";
+import { getTranslations } from "next-intl/server";
+import { trackIssue } from "@/lib/trackIssue";
 
 export const updateWishlistById = async (id: string, formData: FormData) => {
   try {
-    const session = await getServerSession(authOptions)
-    const t = await getTranslations("Errors")
+    const session = await getServerSession(authOptions);
+    const t = await getTranslations("Errors");
 
     if (!session) {
-      throw new Error(t("notLoggedIn"))
+      throw new Error(t("notLoggedIn"));
     }
 
-    const userId = session.user.id
+    const userId = session.user.id;
 
     const wishlist = await prisma.wishlist.findUnique({
       where: {
@@ -28,46 +28,48 @@ export const updateWishlistById = async (id: string, formData: FormData) => {
         userId,
         removed: false,
       },
-    })
+    });
 
     if (!wishlist) {
-      throw new Error(t("wishlist.notFound"))
+      throw new Error(t("wishlist.notFound"));
     }
 
     const data = wishlistSchema.parse({
       name: formData.get("name")?.toString() || undefined,
+      description: formData.get("description")?.toString() || undefined,
       theme: formData.get("theme")?.toString() || undefined,
       groups: formData.getAll("groups").map((group) => group.toString()),
-    })
+    });
 
     await prisma.wishlist.update({
       where: { id },
       data: {
         title: data.name || null,
+        description: data.description || null,
         theme: data.theme || null,
         groups: { set: data.groups?.map((id) => ({ id })) },
       },
       select: wishlistProperties,
-    })
+    });
 
-    revalidatePath("/")
+    revalidatePath("/");
 
     return {
       type: "success" as const,
-    }
+    };
   } catch (error) {
     if (error instanceof z.ZodError) {
       return {
         type: "error" as const,
         errors: error.issues.map((issue) => issue.message),
-      }
+      };
     }
 
-    trackIssue("Update wishlist by ID", "error", { error })
+    trackIssue("Update wishlist by ID", "error", { error });
 
     return {
       type: "error" as const,
       errors: [getErrorMessage(error)],
-    }
+    };
   }
-}
+};
