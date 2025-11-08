874a03fcc54d2bbc9da4337a9708a3c8797ab27d
Ulla <ullavansprang@gmail.com>
2025-11-08 20:27:58 +0100

    feat: use popover on infinity icon

--- src/components/UnlimitedItem/index.tsx
+++ src/components/UnlimitedItem/index.tsx
@@ -1,13 +1,20 @@
 import { InfinityIcon } from "lucide-react"
 import { useTranslations } from "next-intl"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 
 export const UnlimitedItem = () => {
   const t = useTranslations("Toggle")
 
   return (
     <div className="absolute right-3">
-      <InfinityIcon />
-      {/* <p>{t("unlimited")}</p> */}
+      <Popover>
+        <PopoverTrigger>
+          <InfinityIcon />
+        </PopoverTrigger>
+        <PopoverContent>
+          <p>{t("unlimited")}</p>
+        </PopoverContent>
+      </Popover>
     </div>
   )
 }
